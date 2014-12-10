d5db197efa158bdd575f7e057b80a7e8138d19e2
Ali Darwash <alidarwash@MacBook-Pro.local>
2014-12-10 18:08:49 +0000

    Simple file uploader

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Uploader</title>
+	</head>
+	<body>
+		<form action="upload.php" method="post" enctype="multipart/form-data">
+			<input type="file" name="file">
+			<input type="submit" value="Submit">
+			
+		</form>
+	</body>
+</html>
